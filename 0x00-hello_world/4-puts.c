e68577d36e6e81b4643e0302e931a0496a70b043
edmuhumuza <muhumuzaedgar19@gmail.com>
2022-09-09 04:39:49 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	/*string initialisation*/
