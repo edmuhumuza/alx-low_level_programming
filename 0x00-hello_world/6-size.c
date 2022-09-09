bbae24fb9a6b12705e6df8e00920fe4738ced40b
edmuhumuza <muhumuzaedgar19@gmail.com>
2022-09-09 07:02:44 -0700

    not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,11 +17,11 @@ int main(void)
 
 
 	/*size of evaluates the size of a variable*/
-	printf("Size of char: %zu byte\n", sizeof(charType));
-	printf("Size of int: %zu bytes\n", sizeof(intType));
-	printf("Size of long int: %zu bytes\n", sizeof(longType));
-	printf("Size of long long int: %zu bytes\n", sizeof(longlongType));
-	printf("Size of float: %zu bytes\n", sizeof(floatType));
+	printf("Size of a char: %zu byte\n", sizeof(charType));
+	printf("Size of an int: %zu bytes\n", sizeof(intType));
+	printf("Size of a long int: %zu bytes\n", sizeof(longType));
+	printf("Size of a long long int: %zu bytes\n", sizeof(longlongType));
+	printf("Size of a float: %zu bytes\n", sizeof(floatType));
 
 
 	return (0);
